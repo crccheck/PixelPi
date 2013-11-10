353cfca2ff10ca8de223f34751d0ef2df919275d
crccheck <c@crccheck.com>
2013-11-10 08:28:40 -0600

    add some BDD style comments to tests

--- pixelpi_tests.py
+++ pixelpi_tests.py
@@ -31,37 +31,44 @@ class BaseStripTest(unittest.TestCase):
         self.strip = DummyStrip()
 
     def test_chip_type_attribute_comes_from_class(self):
+        # Should know the chip type
         self.assertEqual(self.strip.chip_type, 'DUMMY9000')
         strip = DummyStrip(chip_type='LOL1337')
         self.assertEqual(strip.chip_type, 'DUMMY9000')
 
     def test_pixel_size_attribute(self):
+        # Should know how many bytes are in a pixel
         self.assertEqual(self.strip.pixel_size, 3)
 
     def test_num_leds_can_be_set(self):
+        # Should know how many leds are in the strand
         strip = DummyStrip(num_leds=99)
         self.assertEqual(strip.num_leds, 99)
 
     def test_refresh_rate_can_be_set(self):
+        # Should be able to set a refresh rate
+        # DELETEME this is an implementation detail
         strip = DummyStrip(refresh_rate=501)
         self.assertEqual(strip.refresh_rate, 501)
 
     def test_spi_dev_name_default(self):
+        # Should be able to set the SPI device
         self.assertEqual(self.strip.spidev.name, '/dev/spidev0.0')
-
-    def test_spi_dev_name_can_be_set(self):
         strip = DummyStrip(spi_dev_name='foobar')
-        # TODO
         self.assertEqual(strip.spidev.name, 'foobar')
 
     def test_write_stream_args(self):
+        # Should be able to write data to the SPI device
         self.strip.write_stream('some data')
         self.assertEqual(self.strip.spidev.pixels, 'some data')
 
     def test_gamma(self):
+        # Should be able to set the gamma lookup table
+        # WISHLIST separate gamma for R, G, B
         self.assertEqual(len(self.strip.gamma), 256)
 
     def test_filter_pixel(self):
+        # Should be able to process a pixel from human friendly to chip friendly
         self.assertEqual(self.strip.filter_pixel(WHITE, 1), WHITE)
         self.assertEqual(self.strip.filter_pixel(WHITE, 0.502), GRAY)  # XXX
         self.assertEqual(self.strip.filter_pixel(WHITE, 0), BLACK)
